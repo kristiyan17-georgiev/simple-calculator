20661827c9ecb7c8e0904df61558784617a1e623
kristiyan17-georgiev <20117@uktc-bg.com>
2025-02-21 08:19:36 +0200

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+from simple_calculator.calculator import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_add_floats():
+    assert add(0.1, 0.2) == 0.3
+
